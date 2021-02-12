1b029cef11cb9a3281171ed31ba2ed13de6cfa64
mr-FastWalker <mr.skorokhod@mail.ru>
2021-02-12 21:19:03 +1000

    auto fill massive data with MAP

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,36 +1,38 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import {NavLink} from "react-router-dom";
-
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.DialogId;
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
-
-const Message = (props) => {
-    return <div className="message">{props.text}</div>
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
+
+    let DialogsData = [
+        {id: 1, name: 'Serg'},
+        {id: 2, name: 'Sveta'},
+        {id: 3, name: 'Lex'},
+        {id: 4, name: 'Olya'},
+        {id: 5, name: 'Miha'}
+    ];
+
+    let messagesData = [
+        {id: 1, text: 'Привет!'},
+        {id: 2, text: 'Как дела?'},
+        {id: 3, text: 'Тренируемся печатать буквы'},
+        {id: 4, text: 'расскажи анекдот'},
+        {id: 5, text: 'новое сообщение'}
+    ];
+
+    let DialogElements = DialogsData.map ( Dialog => <DialogItem name={Dialog.name} DialogId={Dialog.id} /> );
+    let MessagesElements = messagesData.map ( message => <Message text={ message.text}/> );
+
     return (
         <div className={s.dialogs}>
 
             <div className={s.dialogsItems}>
-                <DialogItem name='Serg' DialogId='1' />
-                <DialogItem name='Sveta' DialogId='2' />
-                <DialogItem name='Lex' DialogId='3' />
-                <DialogItem name='Olya' DialogId='4' />
-                <DialogItem name='Miha' DialogId='5' />
+                {DialogElements}
             </div>
 
             <div className="messages">
-                <Message text='Привет!'/>
-                <Message text='Как дела?'/>
-                <Message text='Тренируемся печатать буквы'/>
-                <Message text='расскажи анекдот'/>
-                <Message text='новое сообщение'/>
+                {MessagesElements}
             </div>
 
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,15 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post.jsx'
 
 const MyPosts = () => {
+
+    let TextPostData = [
+        {id:1, textPost:'Привет! Это первый пост', Likes:44},
+        {id:2, textPost:'А это второй пост', Likes:56},
+        {id:3, textPost:'Третий пост до кучи', Likes:8},
+    ];
+
+    let Posts = TextPostData.map (p => <Post TextPost={p.textPost} Likes={p.Likes}/>);
+
     return (
         <div>
             <div>
@@ -15,10 +24,11 @@ const MyPosts = () => {
             <div className={classes.posts}>
                 <h2>My posts</h2>
 
-                <Post TextPost='Привет! Это первый пост' Likes='44'/>
-                <Post TextPost='А это второй пост' Likes='56'/>
-                <Post TextPost='Третий пост до кучи' Likes='0'/>
-                <Post/>
+                {Posts}
+
+                {/*<Post TextPost={TextPostData[0].textPost} Likes={TextPostData[0].Likes}/>*/}
+                {/*<Post TextPost='А это второй пост' Likes='56'/>*/}
+                {/*<Post TextPost='Третий пост до кучи' Likes='0'/>*/}
 
             </div>
         </div>
