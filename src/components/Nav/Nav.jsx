b5e67fbb6eb67200f082da52f60812ff648cd683
mr-FastWalker <mr.skorokhod@mail.ru>
2021-03-13 16:08:23 +1000

    53 class component Users created

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
     return (
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink to='/profile' activeClassName={s.active} a> Profile</NavLink>
+                <NavLink to='/profile' activeClassName={s.active}> Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
                 <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
@@ -17,6 +17,9 @@ const Nav = () => {
             </div>
             <div className={s.item}>
                 <NavLink to='/Music' activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/Users' activeClassName={s.active}>Users</NavLink>
             </div>
             <div className={s.item}>
                 <NavLink to='/Settings' activeClassName={s.active}>Settings</NavLink>
