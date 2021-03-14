45c9e8d64b017f22fdbf99b11cb4a5d4a60a0a3a
mr-FastWalker <mr.skorokhod@mail.ru>
2021-03-14 22:59:45 +1000

    54 componentDidMount used

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -45,9 +45,7 @@ import userPhoto from '../../assets/images/avatarUserNull.jpg';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios
             .get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
